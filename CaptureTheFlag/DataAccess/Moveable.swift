c31ac9f3f81dc2cd41d42d6701fd6f732a9e4f09
Ethan Abrams <ethana56@Ethans-MacBook-Pro.local>
2019-04-09 17:59:11 -0700

    cleaned up file

--- CaptureTheFlag/DataAccess/Moveable.swift
+++
@@ -1,8 +0,0 @@
-
-import Foundation
-protocol Moveable {
-    var location: Location? {get set}
-    var id: String {get}
-    var name: String? {get set}
-}
-
--- CaptureTheFlag/GameLogic/GameLocationManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GameLocationManager.swift
-//  CaptureTheFlag
-//
-//  Created by Quinn Coleman on 3/11/18.
-//  Copyright © 2018 Joe Durand. All rights reserved.
-//
-
-import Foundation
